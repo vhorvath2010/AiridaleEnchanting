27b3ae3380656e14032405366120eab28f0454e4
Vincent Horvath <vhorvath2010@gmail.com>
2020-11-30 15:07:12 -0500

    Initial commit

---
+++ src/main/java/com/vhbob/airienchanting/AiriEnchanting.java
@@ -0,0 +1,11 @@
+package com.vhbob.airienchanting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AiriEnchanting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+}
